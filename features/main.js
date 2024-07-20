8ad7cc3deed110c229105779776fdf1e36079aa8
jathurT <jathurshan_t_e23@engug.ruh.ac.lk>
2024-07-20 20:36:11 +0530

    Code updated

--- features/main.js
+++ features/main.js
@@ -1,24 +1,22 @@
-const list = document.querySelectorAll('ul li')
-
+const list = document.querySelectorAll("ul li");
 
 // add active list to sidebar links list.
-list.forEach( li => {
-  li.addEventListener('click', e =>{
-    list.forEach( li => {
-      li.classList.remove('active')
-    })
-    e.currentTarget.classList.add('active')
-  })
-})
-
+list.forEach((li) => {
+  li.addEventListener("click", (e) => {
+    list.forEach((li) => {
+      li.classList.remove("active");
+    });
+    e.currentTarget.classList.add("active");
+  });
+});
 
 // chart 0
 var xArray1 = ["Completed", "Pending"];
 var yArray1 = [55, 45];
 
-var layout1 = {title:"Saving Status"};
+var layout1 = { title: "Saving Status" };
 
-var data1 = [{labels:xArray1, values:yArray1, type:"pie"}];
+var data1 = [{ labels: xArray1, values: yArray1, type: "pie" }];
 
 Plotly.newPlot("chart-0", data1, layout1);
 
@@ -45,45 +43,43 @@ Plotly.newPlot("chart-0", data1, layout1);
 
 // Plotly.newPlot("chart-2", data2, layout2);
 
-
-
-
 // chart 3
 
-var xArray4 = [1,2,3,4,5,6,7,10,14,15,18,24,25,26,27,28,29,30,31];
-var yArray4 = [100,100,20,90,19,9,10,20,14,14,151,140,140,104,114,134,150,0,14,100];
+var xArray4 = [
+  1, 2, 3, 4, 5, 6, 7, 10, 14, 15, 18, 24, 25, 26, 27, 28, 29, 30, 31,
+];
+var yArray4 = [
+  100, 100, 20, 90, 19, 9, 10, 20, 14, 14, 151, 140, 140, 104, 114, 134, 150, 0,
+  14, 100,
+];
 
 // Define Data
-var data4 = [{
-  x: xArray4,
-  y: yArray4,
-  mode:"markers",
-  type:"scatter"
-}];
+var data4 = [
+  {
+    x: xArray4,
+    y: yArray4,
+    mode: "markers",
+    type: "scatter",
+  },
+];
 
 // Define Layout
 var layout4 = {
-  xaxis: {range: [0, 31], title: "Days"},
-  yaxis: {range: [0, 500], title: "Amount"},
-  title: "Daily Expenditure"
+  xaxis: { range: [0, 31], title: "Days" },
+  yaxis: { range: [0, 500], title: "Amount" },
+  title: "Daily Expenditure",
 };
 
 Plotly.newPlot("chart-4", data4, layout4);
 
-const navItem = document.querySelectorAll('li');
+const navItem = document.querySelectorAll("li");
 
-
-
-navItem.forEach(navItem => {
-
-    navItem.addEventListener('click', () => {
-
-        navItem.forEach(navItem => {
-            navItem.classList.remove('active');
-        });
-
-        navItem.classList.add('active');
-
+navItem.forEach((navItem) => {
+  navItem.addEventListener("click", () => {
+    navItem.forEach((navItem) => {
+      navItem.classList.remove("active");
     });
 
+    navItem.classList.add("active");
+  });
 });
